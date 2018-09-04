901eb4683568bc478f113d943d6ea99d395c41e2
Kubing Lin <t6m8@hotmail.com>
2018-09-04 21:30:17 +0800

    add helper and provider

---
+++ EPPHelper.Core/Providers/IObjectResolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace EPPHelper.Core
+{
+    /// <summary>
+	/// Defines the functionality of a class that creates objects
+	/// from a given type.
+	/// </summary>
+	public interface IObjectResolver
+    {
+        /// <summary>
+        /// A value indicating if the resolver's <see cref="CanResolve"/>
+        /// returns false that an object will still be created using
+        /// CsvHelper's object creation. True to fallback, otherwise false.
+        /// Default value is true.
+        /// </summary>
+        bool UseFallback { get; }
+
+        /// <summary>
+        /// A value indicating if the resolver is able to resolve
+        /// the given type. True if the type can be resolved,
+        /// otherwise false.
+        /// </summary>
+        Func<Type, bool> CanResolve { get; }
+
+        /// <summary>
+        /// The function that creates an object from a given type.
+        /// </summary>
+        Func<Type, object[], object> ResolveFunction { get; }
+
+        /// <summary>
+        /// Creates an object from the given type using the <see cref="ResolveFunction"/>
+        /// function. If <see cref="CanResolve"/> is false, the object will be
+        /// created using CsvHelper's default object creation. If <see cref="UseFallback"/>
+        /// is false, an exception is thrown.
+        /// </summary>
+        /// <param name="type">The type to create an instance from. The created object
+        /// may not be the same type as the given type.</param>
+        /// <param name="constructorArgs">Constructor arguments used to create the type.</param>
+        object Resolve(Type type, params object[] constructorArgs);
+
+        /// <summary>
+        /// Creates an object from the given type using the <see cref="ResolveFunction"/>
+        /// function. If <see cref="CanResolve"/> is false, the object will be
+        /// created using CsvHelper's default object creation. If <see cref="UseFallback"/>
+        /// is false, an exception is thrown.
+        /// </summary>
+        /// <typeparam name="T">The type to create an instance from. The created object
+        /// may not be the same type as the given type.</typeparam>
+        /// <param name="constructorArgs">Constructor arguments used to create the type.</param>
+        T Resolve<T>(params object[] constructorArgs);
+    }
+}
